8bf93b84dbca36510f158ebb028f5dab2b0f5042
ItoKobi <ito.kobilol@gmail.com>
2021-06-18 01:00:14 -0400

    Alot of new stuff ig

--- script.js
+++ script.js
@@ -7,19 +7,61 @@ const tablesAmount = document.getElementById("tables");
 const chairsAmountMobile = document.getElementById("chairs-mobile");
 const tablesAmountMobile = document.getElementById("tables-mobile");
 
+const mainbody = document.body;
 const mainSection = document.getElementById("mainsection");
 const mustachio = document.querySelectorAll(".draggable");
-const allTesting = document.querySelectorAll(".draggable");
+const allTesting = document.getElementsByClassName("draggable");
 const imageDownload = document.getElementById("image");
-const saveButton = document.getElementById("saveButton");
+const lockButton = document.getElementById("lock");
 const maxChairs = 100;
 const maxTables = 13;
 let chairsCount = 0;
 let tablesCount = 0;        
 let downloadImageURL;
+let bodyActions = false;
+
+
+const addBoxesPrompt = confirm("editable boxes?");
+
+function setBodyActions(){
+    if(bodyActions === true){
+        mainbody.style.touchAction = "auto"
+        bodyActions = false;
+        lockButton.style.color = "white"
+    }else{
+        mainbody.style.touchAction = "none"
+        bodyActions = true;
+        lockButton.style.color = "red"
+    }
+}
+
+if(addBoxesPrompt){
+    document.getElementById("image").innerHTML += `<div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 106.6px; top: 212.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 106.6px; top: 231.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 86.6px; top: 213.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 11.7875px; top: 449.788px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 187px; top: 376px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 13.6px; top: 389.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 206.6px; top: 345.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 230.6px; top: 346.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 231.6px; top: 449.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 194.6px; top: 448.6px;"></div><div class="bg-red-700 absolute shadow-lg w-10 h-6 cursor-pointer hover:scale-105 ease-in transform ease-out draggable table-element ui-draggable ui-draggable-handle" style="left: 150px; top: 450.4px;"></div><div class="bg-red-700 absolute shadow-lg w-10 h-6 cursor-pointer hover:scale-105 ease-in transform ease-out draggable table-element ui-draggable ui-draggable-handle" style="left: 108px; top: 451.4px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 89.6px; top: 449.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 139.6px; top: 434.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 264.6px; top: 604.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 231px; top: 515px;"></div>`
+    
+    $(".chair-element").draggable();
+    $(".table-element").draggable();
+
+    $(".chair-element").dblclick(function(){
+        $(this).remove();
+    });
+
+    $(".table-element").dblclick(function(){
+        $(this).remove();
+    });
+    
+    for(let i = 0; allTesting.length > i; i++){
+        allTesting[i].addEventListener('touchmove', (ev) => {
+            let touchLocation = ev.targetTouches[0];
+            allTesting[i].style.left = parseInt(touchLocation.pageX) + 'px';
+            allTesting[i].style.top = parseInt(touchLocation.pageY) + 'px';
+        })
+    }
+    
+}else{
+    document.getElementById("image").innerHTML += `<div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 106.6px; top: 212.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 106.6px; top: 231.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 86.6px; top: 213.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 11.7875px; top: 449.788px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 187px; top: 376px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 13.6px; top: 389.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 206.6px; top: 345.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 230.6px; top: 346.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 231.6px; top: 449.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 194.6px; top: 448.6px;"></div><div class="bg-red-700 absolute shadow-lg w-10 h-6 cursor-pointer hover:scale-105 ease-in transform ease-out draggable table-element ui-draggable ui-draggable-handle" style="left: 150px; top: 450.4px;"></div><div class="bg-red-700 absolute shadow-lg w-10 h-6 cursor-pointer hover:scale-105 ease-in transform ease-out draggable table-element ui-draggable ui-draggable-handle" style="left: 108px; top: 451.4px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 89.6px; top: 449.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 139.6px; top: 434.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 264.6px; top: 604.6px;"></div><div class="chair-element bg-red-700 shadow-lg w-4 h-4 absolute cursor-pointer hover:scale-105 ease-in transform ease-out draggable ui-draggable ui-draggable-handle" style="left: 231px; top: 515px;"></div>`
+}
 
 function saveTest(){
-    saveButton.style.display = "none";
     domtoimage.toPng(document.getElementById("image")).then(function (dataUrl) {
         setImage(dataUrl);
     });
@@ -57,7 +99,6 @@ function addChair(){
     })
 }
 
-
 function addTable(){
     
     if(maxTables === tablesCount){
@@ -78,8 +119,11 @@ function addTable(){
         const transformStyle = this.style.transform
         if(transformStyle === "rotate(90deg)"){
             $(this).css("transform", "rotate(180deg)");
+        }
+        else if(transformStyle === "rotate(45deg)"){
+
         }else{
-            $(this).css("transform", "rotate(90deg)");
+            $(this).css("transform", "rotate(45deg)");
         }
     })
 
@@ -158,9 +202,6 @@ function removeImageDisplay(){
     mainSection.style.display = "grid";
     imageWrapper.style.display = "none";
     imageDisplay.style.display = "none";
-    if(screen.width > 600){
-        saveButton.style.display = "block";
-    }
 }
 
 function downloadImage(){
